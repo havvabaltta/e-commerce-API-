0bbda3b47192eac82b39c8b9d9e5be405dde075d
havvabaltta <104566708+havvabaltta@users.noreply.github.com>
2026-05-02 20:10:10 +0300

    frontend yapı

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand">E-Commerce</a>
+      </div>
+    </nav>
+  );
+}
